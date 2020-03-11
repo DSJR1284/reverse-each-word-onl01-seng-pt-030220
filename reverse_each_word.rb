fcd557a1ac20aaf39442d26bb4a67b68d2605759
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 23:45:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,19 @@
 #       return phrase.reverse
 # end 
 
+# def reverse_each_word(phrase)
+#   new_phrase=[]
+#   phrase.split( ).each do |line|
+#     new_phrase << line.reverse 
+#   end 
+#   return new_phrase.join(" ") 
+# end 
+
+
 def reverse_each_word(phrase)
   new_phrase=[]
-  phrase.split( ).each do |line|
+  phrase.split( ).collect do |line|
     new_phrase << line.reverse 
   end 
    return new_phrase.join(" ") 
 end 
-
-
