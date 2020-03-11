ef59f9a7396a1532d9c2c60bf10a6a2353e764b6
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 23:30:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
-phrase = "Hello there, and how are you?"
 
-def reverse_each_word(phrase)
-      return phrase.reverse
+
+# def reverse_each_word(phrase)
+#       return phrase.reverse
+# end 
+
+def reverse(phrase)
+  new_phrase=[]
+  phase.each do |line|
+    new_phrase << line.reverse 
+  end 
+  return  new_phrase 
 end 
-
